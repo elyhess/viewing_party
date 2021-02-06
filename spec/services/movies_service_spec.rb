cb7075469d9f0dbdf2d7b318e23bfc257294dfaf
foymikek <>
2021-02-06 15:58:44 -0700

    WIP movies_servies testing
    Co-authored-by: Robert Heath <68252946+kaiheiongaku@users.noreply.github.com>
    Co-authored-by: Ely Hess <46981974+elyhess@users.noreply.github.com>

--- spec/services/movies_service_spec.rb
+++ spec/services/movies_service_spec.rb
@@ -6,4 +6,15 @@ describe MoviesService do
 
     expect(movies_services).to be_an_instance_of(MoviesService)
   end
+
+  it 'can search by movie', :disable_vcr do
+    pulp_fiction_response = File.read("spec/fixtures/movies_services/pulp_fiction_movie_search_query.json")
+    
+    stub_request(:get, "https://api.themoviedb.org/3/search/movie?query='pulp%20fiction'").
+    to_return(status: 200, body: pulp_fiction_response)
+    
+    pulp_fiction = MoviesService.search_by_movie("Pulp Fiction")
+    require 'pry'; binding.pry
+    # expect(pulp_fiction[:]).to eq() 
+  end
 end
