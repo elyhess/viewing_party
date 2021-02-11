3ad1f658af8e17b309e1acf64688937805fc9e15
Robert Heath <robert.b.heathii@gmail.com>
2021-02-10 21:05:33 -0600

    Rubocop revenge

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,16 @@ class User < ApplicationRecord
                     format: { with: /\A[^@\s]+@[^@\s]+\z/, message: ', you must use a valid email address' }
   validates :password, presence: true
 
-  has_many :friendships
+  has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships, source: :friend
-  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :inverse_friendships,
+           class_name: 'Friendship',
+           foreign_key: 'friend_id',
+           dependent: :destroy,
+           inverse_of: :friend
   has_many :inverse_friends, through: :inverse_friendships, source: :user
-  has_many :parties
-  has_many :party_guests
+  has_many :parties, dependent: :destroy
+  has_many :party_guests, dependent: :destroy
 
   has_secure_password
 
